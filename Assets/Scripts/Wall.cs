13f140595981435c69f59e80fb6868e00ac9df72
liufei <feiliu@faceunity.com>
2021-01-26 17:44:45 +0800

    更新下动画状态机

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Wall : MonoBehaviour
+{
+    public Sprite HitedSprite;
+    public int hp = 5;
+
+    private SpriteRenderer render;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        render = GetComponent<SpriteRenderer>();
+    }
+
+    public void DamageWall(int loss)
+    {
+        render.sprite = HitedSprite;
+        hp -= loss;
+        if(hp <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
